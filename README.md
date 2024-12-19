50ee1699a6438c8f4ba13eb7cda10861a660ce75
Natasha C <n.coward@hotmail.com>
2024-12-19 17:01:21 +0000

    Managed to fetch API data and display it on the page.

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 Cookie Clicker Assignment
 
-Started by copying Joe's walking skeleton.
-Changed background in CSS file, tried to put all the div elements into a grid/rows.
-Went over the Fetch API workshop and created console.js to work on the workshop as a reminder.
-Started to apply the same logic from Fetch API workshop into the app.js file.
-I can see the returned JSON data in the console log.
+- Started by copying Joe's walking skeleton.
+- Changed background in CSS file, tried to put all the div elements into a grid/rows.
+- Went over the Fetch API workshop and created console.js to work on the workshop as a reminder.
+- Started to apply the same logic from Fetch API workshop into the app.js file.
+- I can see the returned JSON data in the console log.
+- MANAGED TO GET THE API DATA TO SHOW ON THE PAGE!! (With, full disclosure, some help of google results nand Chat GPT to debug my code. It was useful to show that I was defining things too early and let me know if I'm missing (), {}, ; etc).
+-
--- app.js
+++ app.js
@@ -31,12 +31,20 @@ async function getShopUpgrades() {
   console.log("HTTP Response:", result);
   const json = await result.json();
   console.log("JSON Data:", json);
+  // //The following lines were written with the help of ChatGPT:
+  json.forEach((upgrade) => {
+    const { id, cost, name, increase } = upgrade;
+    const upgradeElement = document.createElement("div");
+    upgradeElement.classList.add("shop", "item");
 
-  //The following 4 lines were written with the help of ChatGPT:
-  const cookieUpgradeName = json.name;
-  const cookieElement = document.createElement("div");
-  cookieElement.textContent = `${cookieUpgradeName}`;
-  document.body.appendChild(cookieElement);
+    upgradeElement.innerHTML = `
+    <div class="id">${id}</div>
+    <div class="name">${name}</div>
+    <div class="cost">$${cost}</div>
+    <div class="increase">+${increase}</div>
+`;
+    upgradesContainer.appendChild(upgradeElement);
+  });
 }
 
 getShopUpgrades();
@@ -49,36 +57,36 @@ getShopUpgrades();
 //PUSH the upgrade data from the fetch into the empty shopUpgradesArray
 //Remember that the upgrades that we fetched will be known by a variable name that you have set on line 33-35 (return)
 
-async function renderShopUpgrades() {
-  const getShopItems = await getShopUpgrades();
-  //Now that we have our upgrade items inside an array we can perform array method on them! Just like we did for images in the WK2 gallery submission!
+// async function renderShopUpgrades() {
+//   const getShopItems = await getShopUpgrades();
+//   //Now that we have our upgrade items inside an array we can perform array method on them! Just like we did for images in the WK2 gallery submission!
 
-  getShopItems.forEach(function (upgrade) {
-    //This is where you should go back to Week 2 gallery loop and apply the same logic to this loop
-    //const upgradeName = [**************FINISH THIS CODE***************], like when we created the thumbnail value in week 2.
-    //const upgradeCost = [**************FINISH THIS CODE***************]
-    //const upgradeCPSIncrease = [**************FINISH THIS CODE***************]
-    //These elements should be ones that just display text ee.g. <p> or <h1>
-    //These are brand new elements that don't contain any content, so we need to assign content to them. Exactly the same strategy as when you assigned a src and alt to the thumbnail images in week 2.
-    // instead of giving these elements a src and alt, we want to give them some **TEXTCONTENT** <- a breadcrumb to follow.
-    //Append these elements into the relevant container, in the same way as you appended your images into the thumbnail container in week 2.
-    //This is a decent place to also create a button element and attach an event listener to it. You will then need to create a handler function for the button that you create.
-  });
-}
+//   getShopItems.forEach(function (upgrade) {
+//     //This is where you should go back to Week 2 gallery loop and apply the same logic to this loop
+//     //const upgradeName = [**************FINISH THIS CODE***************], like when we created the thumbnail value in week 2.
+//     //const upgradeCost = [**************FINISH THIS CODE***************]
+//     //const upgradeCPSIncrease = [**************FINISH THIS CODE***************]
+//     //These elements should be ones that just display text ee.g. <p> or <h1>
+//     //These are brand new elements that don't contain any content, so we need to assign content to them. Exactly the same strategy as when you assigned a src and alt to the thumbnail images in week 2.
+//     // instead of giving these elements a src and alt, we want to give them some **TEXTCONTENT** <- a breadcrumb to follow.
+//     //Append these elements into the relevant container, in the same way as you appended your images into the thumbnail container in week 2.
+//     //This is a decent place to also create a button element and attach an event listener to it. You will then need to create a handler function for the button that you create.
+//   });
+// }
 
-renderShopUpgrades();
+// renderShopUpgrades();
 
-//We want our upgrades to actually do something. We need to give each upgrade a button and attach an event listener to those buttons.
+// //We want our upgrades to actually do something. We need to give each upgrade a button and attach an event listener to those buttons.
 
-function handleUpgradeClick() {
-  //The logic for the button to only deal with the values of the specific upgrade it was created for is the same logic as the event handler for the creation of the large image element in the week 2 assignment.
-  //Here is a great place to include some logic that checks a conditional to see if you have enough cookies in cookieCount to be able to afford the price of the upgrade. If you can't afford an upgrade, how can you give this feedback to your user? console log? render a new DOM element 'you don't have enough cookies'? trigger alert?
-}
+// function handleUpgradeClick() {
+//   //The logic for the button to only deal with the values of the specific upgrade it was created for is the same logic as the event handler for the creation of the large image element in the week 2 assignment.
+//   //Here is a great place to include some logic that checks a conditional to see if you have enough cookies in cookieCount to be able to afford the price of the upgrade. If you can't afford an upgrade, how can you give this feedback to your user? console log? render a new DOM element 'you don't have enough cookies'? trigger alert?
+// }
 
-setInterval(function () {
-  //We want our timer to increase the value of cookieCount by the value of cookiesPerSecond every second.
-  //We want to update the cookieCount value on our page as it changes.
-  //I want to store this value in local storage so that my user can resume the game with their game data intact.
-}, 1000);
+// setInterval(function () {
+//   //We want our timer to increase the value of cookieCount by the value of cookiesPerSecond every second.
+//   //We want to update the cookieCount value on our page as it changes.
+//   //I want to store this value in local storage so that my user can resume the game with their game data intact.
+// }, 1000);
 
-//It is perfectly fine to perform all of the actions that we want our setInterval function to do with external functions, then you can callback those functions inside the setInterval function.
+// //It is perfectly fine to perform all of the actions that we want our setInterval function to do with external functions, then you can callback those functions inside the setInterval function.
